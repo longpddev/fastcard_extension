7f5cfa2ed27c7dc657006f8b91cd655d1bac7f88
longpd <longpddev@gmail.com>
2022-09-12 22:09:12 +0700

    impovement script

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.get("/userscript", async (req, res) => {
 
   (function () {
       "use strict";
-      const host = "${host}:3000";
+      const host = "${host}:${port}";
       if(unsafeWindow.location.host === host) return;
       window.fetchAPI = GM_fetch;
       window.setStorageValue = GM_setValue;
