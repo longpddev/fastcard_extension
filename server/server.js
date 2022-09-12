b2bda6563fef5653dabbf235c952904cdc29ce73
longpd <longpddev@gmail.com>
2022-09-12 10:40:50 +0700

    create static file

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const fs = require("fs");
 const port = 6600;
 app.use(cors());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   const path = __dirname.split("/");
@@ -26,28 +27,35 @@ app.get("/userscript", (req, res) => {
     // @version      0.1
     // @description  try to take over the world!
     // @author       You
-    // @match        *
+    // @match      *://*/*
     // @grant    GM_registerMenuCommand
-    // @grant        GM_addValueChangeListener
+    // @grant        GM_xmlhttpRequest
     // @grant        GM_log
     // @grant        GM_setValue
     // @grant        GM_getValue
     // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
     // ==/UserScript==
-
-    (function() {
-        'use strict';
-        GM_addValueChangeListener('long', function(name, old_value, new_value, remote) {
-          GM_log('listener', {name, old_value, new_value, remote})
-        })
-    GM_registerMenuCommand('show config', function () {
-        GM_log('log', GM_getValue('long'))
-    });
-    GM_registerMenuCommand('set config', function () {
-        GM_setValue('long', Math.random() * 1000)
-    });
-
-        // Your code here...
+    
+    (function () {
+      'use strict';
+      const script = document.createElement('script')
+      const fastCard = document.createElement('fast-card-body')
+      document.body.append(fastCard)
+      document.body.append(script);
+    
+      document.body.addEventListener('fastCardLoaded', () => {
+        fastCard.setPosition({ top: 100, left: 100 });
+        fastCard.setShow(true);
+      })
+    
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: "http://139.162.50.214:6600",
+        onload: (data) => {
+          eval(data.responseText)
+        }
+      })
+      // Your code here...
     })();
   `);
 });
