90c5ff1a06e84772c97e29271090dcea3d8ebd2a
longpd <longpddev@gmail.com>
2022-10-04 01:02:12 +0700

    update domain server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 const fs = require("fs");
 const port = 6600;
-const host = "https://server.longpddev.xyz";
+const host = "http://139.162.50.214";
 app.use(cors());
 app.use("/public", express.static(__dirname + "/public"));
 function getPageFileJs() {
